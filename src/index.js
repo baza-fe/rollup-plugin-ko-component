834cf62cf5797bbf51dd554cd3bcbabd2e8b78fd
BinRui Guan <binrui.guan@hirede.com>
2016-09-01 21:00:49 +0800

    Add transformer

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import { parse } from 'path';
+import { createFilter } from 'rollup-pluginutils';
+import babelCore from 'babel-core';
+import component from 'babel-plugin-ko-component';
+
+export default function plugin(options = {}) {
+    const filter = createFilter(options.include || [ '**/*.js' ], options.exclude || 'node_modules/**');
+
+    return {
+        transform(code, id) {
+            if (!filter(id)) {
+                return null;
+            }
+
+            const labelIdentifierName = '__ko_component_label__';
+            const styleIdentifierName = '__ko_component_style__';
+            const templateIdentifierName = '__ko_component_template__';
+            const labelIdentifierValue = `'${parse(id).name}'`;
+            const styleIdentifierValue = `''`;
+            const templateIdentifierValue = `''`;
+
+            code = [
+                `const ${labelIdentifierName} = ${labelIdentifierValue};`,
+                `const ${styleIdentifierName} = ${styleIdentifierValue};`,
+                `const ${templateIdentifierName} = ${templateIdentifierValue};`,
+                code
+            ].join('\n');
+
+            try {
+                return {
+                    code: babelCore.transform(code, { plugins: component }).code,
+                    map: { mappings: '' }
+                };
+            } catch (error) {
+                throw error;
+            }
+        }
+    };
+};
